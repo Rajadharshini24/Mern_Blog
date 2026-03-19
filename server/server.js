a210d3dbf92d144d599b7545ebce729c24543011
Rajadharshini24 <23cseb22.rajadharshini.r@gmail.com>
2026-03-19 23:53:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const authRoutes=require("./routes/authRoutes");
 const blogRoutes=require("./routes/blogRoutes");
