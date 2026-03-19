98e0b3203e0d74009e598124f381966a410a7db1
Rajadharshini24 <23cseb22.rajadharshini.r@gmail.com>
2026-03-19 23:47:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app=express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL.replace(/\/$/, ""),
   credentials: true,
 }));
 app.use(express.json());
